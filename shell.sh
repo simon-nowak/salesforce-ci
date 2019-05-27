90ffd0ecaa5a7cf967890804854303b7a5c66efb
Doug Ayers <douglascayers@gmail.com>
2019-05-27 02:19:40 -0500

    add package alias so can create package version

--- shell.sh
+++ shell.sh
@@ -2,12 +2,13 @@
 
 test() {
 
-    local username="DevHub"
-    local object="Account"
-    local name="CD656092"
-    local cmd="sfdx force:data:record:get --sobjecttype $object --targetusername $username --where "'"AccountNumber='$name'"'" --json" && (echo $cmd >&2)
-    local output=$($cmd) && (echo $output | jq >&2)
-    echo $(jq -r '.result.Id' <<< $output)
+    local output=""
+    if [ -n "$output" ]; then
+      echo 'found'
+    else
+      echo 'empty'
+    fi
+
 }
 
 x=$(test foo bar)
